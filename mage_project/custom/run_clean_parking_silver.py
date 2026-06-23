bbfe163d0e28be341634d3ced58176851e98858e
Sofia Alouatiq <sofia.alouatiq@gmail.com>
2026-06-23 18:22:06 +0200

    create: ajout de pipeline de données lié aux parking de la région Briançon

---
+++ mage_project/custom/run_clean_parking_silver.py
@@ -0,0 +1,16 @@
+import subprocess
+
+if 'custom' not in globals():
+    from mage_ai.data_preparation.decorators import custom
+
+@custom
+def execute_custom_script(*args, **kwargs):
+    print("🧹 Ordre reçu : Nettoyage des parkings vers Silver...")
+    
+    result = subprocess.run(["python", "scripts/silver_clean_parking.py"], capture_output=True, text=True)
+    print(result.stdout)
+    
+    if result.returncode != 0:
+        raise Exception(f"❌ Erreur lors de l'exécution : {result.stderr}")
+        
+    return {}
