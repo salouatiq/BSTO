77f7b3a2e48bf28d4511fa11f3a09546fe14025c
Sofia Alouatiq <sofia.alouatiq@gmail.com>
2026-06-23 11:42:54 +0200

    create: ajout des blocs silver et gold au pipeline à l'instar du bloc bronze

---
+++ mage_project/custom/run_aggregate_gold.py
@@ -0,0 +1,20 @@
+import subprocess
+
+if 'custom' not in globals():
+    from mage_ai.data_preparation.decorators import custom
+
+@custom
+def execute_custom_script(*args, **kwargs):
+    print("🌍 Ordre reçu : Lancement de l'extraction vers Bronze...")
+    
+    # On lance ton script Python exactement comme tu le ferais dans le terminal
+    result = subprocess.run(["python", "scripts/gold_aggregate.py"], capture_output=True, text=True)
+    
+    # On affiche ce que le script a fait
+    print(result.stdout)
+    
+    # S'il y a eu un problème rouge, on fait échouer le bloc Mage
+    if result.returncode != 0:
+        raise Exception(f"❌ Erreur lors de l'exécution : {result.stderr}")
+        
+    return {}
